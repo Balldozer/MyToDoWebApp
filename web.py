ad4777ab97571cd5184b3f492bfcc03d2486c877
Balldozer <yannick.baldewijns@outlook.be>
2024-10-07 10:04:55 +0200

    Allow html, capitalize todos

--- web.py
+++ web.py
@@ -5,12 +5,13 @@ todos = functions.get_todos()
 
 def add_todo():
     todo = st.session_state["new_todo"]
+    todo = str(todo).capitalize()
     todos.append(todo + "\n")
     functions.store_todos(todos)
 
 st.title("My To-do App")
 st.subheader("This is my to-do list:")
-st.write("This text is to test the web-app.")
+st.write("This app is to improve your <b>productivity</b>.", unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
